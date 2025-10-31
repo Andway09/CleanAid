fd943a268f683b73b288e41ad0edff41a2c3a928
Xyrille Uy <uy.xyrille823@gmail.com>
2025-10-31 17:15:24 +0800

    upload and upload_process

--- controller/upload_process.php
+++ controller/upload_process.php
@@ -184,7 +184,7 @@ try {
     fclose($csvHandle);
 
     // ===== CONVERT TO PARQUET =====
-    $python = 'python';
+    $python = '"' . realpath(__DIR__ . '/../.venv/Scripts/python.exe') . '"';
 
     $converter = realpath(__DIR__ . '/../scripts/convert_to_parquet.py');
     chdir(dirname($converter));
